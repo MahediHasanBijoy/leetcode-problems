6560adb0923f7076f56ea09505de16a84f33c2a1
mahedi hasan bijoy <bijoycse2014@gmail.com>
2023-09-07 01:15:27 +0600

    prb 509 and 79 done

---
+++ problem70.php
@@ -0,0 +1,48 @@
+<?php
+
+// Problem name: Climbing Stairs
+
+/* 
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+
+// Analysing the inputs and outputs we see its a fibonacci sequence, so the result would be a fibonacci number
+
+function climbStairs($n) {
+    $a = 1;
+    $b = 2;
+    if($n==$a){
+        return $a;
+    }else if($n == $b){
+        return $b;
+    }else{
+        for($i=2; $i< $n; $i++){
+            $temp = $a + $b;
+            $a = $b;
+            $b = $temp;
+        }
+        return $b;
+    }
+}
+
+echo climbStairs(6);
